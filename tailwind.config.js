23018d77d67522e2c0fd0fb99522241b5cd3fd69
xxnuo <alexcheery5@gmail.com>
2025-01-16 01:23:53 +0800

    Update wxt.config.js
    feat: update deps

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx}", "./entrypoints/**/*.{js,jsx}"],
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["cupcake", "halloween"],
-    logs: false,
-  },
-};
--- wxt.config.js
+++ wxt.config.js
@@ -1,12 +1,25 @@
 import { defineConfig } from "wxt";
+import tailwindcss from "@tailwindcss/vite";
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
-  vite: () => ({}),
+  vite: () => ({
+    plugins: [tailwindcss()],
+  }),
   manifest: {
     author: "xxnuo",
     name: "浮生梦",
     description: "支持自定义新标签页的 Chrome 扩展，在新标签页上展示中国经典诗词。",
+    action: {
+      default_icon: {
+        16: "icon/16.png",
+        24: "icon/24.png",
+        32: "icon/32.png",
+        48: "icon/48.png",
+        64: "icon/64.png",
+        128: "icon/128.png",
+      },
+      default_title: "浮生梦",
+    },
   },
 });
