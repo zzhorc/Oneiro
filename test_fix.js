c763c02609d4305852dc4e6bd5694ae15632e282
z2h0rc <a15815246565@icloud.com>
2026-09-02 17:21:59 +0800

    chore: remove development-only assets

--- test_fix.js
+++
@@ -1,1 +0,0 @@
-// Empty wait script simply to transition states properly
--- test_hitokoto.js
+++
@@ -1,3 +0,0 @@
-const g = require('sentences-bundle/sentences/g.json');
-const target = g.find(item => item.hitokoto.includes('月色与雪色之间'));
-console.log(target);
